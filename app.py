a0982a9bd9c5def8147942e2fe06528093f2c4d6
Claudio577 <chideki.yoshida@gmail.com>
2026-03-05 12:11:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# carregar modelo
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("model/cifar10_model.keras")
+    return tf.keras.models.load_model("cifar10_model.keras")
 
 model = load_model()
 
@@ -17,19 +16,21 @@ class_names = [
 
 st.title("AI Image Classifier - CIFAR10")
 
-st.write("Upload an image and the AI will classify it.")
-
 uploaded_file = st.file_uploader("Upload Image")
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file).resize((32,32))
-
-    st.image(image, caption="Uploaded Image")
+    st.image(image)
 
     img_array = np.array(image)/255.0
     img_array = np.expand_dims(img_array, axis=0)
 
+    prediction = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(prediction)]
+
+    st.write("Prediction:", predicted_class)
     prediction = model.predict(img_array)
 
     predicted_class = class_names[np.argmax(prediction)]
